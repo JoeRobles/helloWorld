a2dc9ac06dcc02c0fbb0a31cae48e627b5844d25
Super Power <superpower@lumanrique-MAC.local>
2015-06-15 09:53:51 -0700

    first commit

--- helloWorld/ViewController.swift
+++ helloWorld/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var nameLabel: UILabel!
+    
+    @IBOutlet var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonClicked(sender: UIButton) {
+        nameLabel.text = "Hello \(textField.text)"
+        textField.text = ""
+    }
 
 }
 
